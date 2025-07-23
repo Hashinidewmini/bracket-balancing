19d7a51997585b9b60d5c424ae7129fcc34146ec
Hashinidewmini <hashinifdo04@gmail.com>
2025-07-23 11:02:36 +0530

    Fixes the issue

--- bracket balancing.py
+++ bracket balancing.py
@@ -3,9 +3,12 @@ def is_balanced(expression):
     brackets = {')': '(', '}': '{', ']': '['}
 
     for char in expression:
+        if char in brackets.values():  # Opening brackets
+            stack.append(char)
+        elif char in brackets.keys():  # Closing brackets
+            if not stack or stack.pop() != brackets[char]:
+                return False
 
-        print("I like pizza!")
-        print("Hello hello hello")
 
     return len(stack) == 0  # True if all brackets are closed
 
